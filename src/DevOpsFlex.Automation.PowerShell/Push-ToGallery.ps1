c1e3357c0073d8a48bcb937d64d93b7b837fb8d2
djfr <davidferreirarodrigues@gmail.com>
2016-05-10 10:47:39 +0100

    Tweaked the push script and changed the github URLs in the module metadata

--- src/DevOpsFlex.Automation.PowerShell/Push-ToGallery.ps1
+++ src/DevOpsFlex.Automation.PowerShell/Push-ToGallery.ps1
@@ -4,4 +4,5 @@ param
     [string] $ApiKey
 )
 
-Publish-Module -Repository DevOpsFlex -Name $PSScriptRoot\DevOpsFlex.Environments.PowerShell\DevOpsFlex.Environments.PowerShell.psd1 -NuGetApiKey $ApiKey
+#Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/?LinkID=690216&clcid=0x409" -OutFile "C:\ProgramData\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
+Publish-Module -Name $PSScriptRoot\DevOpsFlex.Environments.PowerShell\DevOpsFlex.Environments.PowerShell.psd1 -NuGetApiKey $ApiKey
