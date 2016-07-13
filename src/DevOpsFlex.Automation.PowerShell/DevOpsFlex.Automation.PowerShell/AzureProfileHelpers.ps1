f420a07eb488fb3ca3d87b1a42a2b44f15724aec
djfr <drodrigues@eshopworld.com>
2016-07-13 12:50:13 +0100

    Added a few helper methods around common Azure needs to use in PowerShell profiles.

---
+++ src/DevOpsFlex.Automation.PowerShell/DevOpsFlex.Automation.PowerShell/AzureProfileHelpers.ps1
@@ -0,0 +1,96 @@
+function Reset-Alias
+{
+<#
+.SYNOPSIS
+Creates and alias with replace functionality.
+
+.DESCRIPTION
+Checks if the alias already exists, if it does calls Set-Alias on it, if it doesn't it will create a new alias using New-Alias.
+Relays the alias scope.
+
+.PARAMETER Name 
+Specifies the new alias. You can use any alphanumeric characters in an alias, but the first character cannot be a number.
+
+.PARAMETER Value
+Specifies the name of the cmdlet or command element that is being aliased.
+
+.PARAMETER Scope
+Specifies the scope of the new alias. Valid values are "Global", "Local", or "Script", or a number relative to the current scope (0 through the number of scopes, where 0 is the current scope and 1 is its parent).
+"Local" is the default. For more information, see about_Scopes.
+
+.EXAMPLE
+Reset-Alias list get-childitem
+This command creates an alias named "list" to represent the Get-ChildItem cmdlet on the "Global" scope.
+
+.NOTES
+Currently CmdletBinding doesn't have any internal support built-in.
+#>
+
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true, Position=0)]
+        [string] $Name,
+
+        [parameter(Mandatory=$true, Position=1)]
+        [string] $Value,
+
+        [parameter(Mandatory=$false)]
+        [ValidateSet('Global', 'Local', 'Script')]
+        [string] $Scope = 'Global'
+    )
+
+    if((Get-Alias -Name $Name -Scope $Scope -ErrorAction SilentlyContinue) -eq $null) {
+        New-Alias -Name $Name -Value $Value -Scope $Scope
+    }
+    else {
+        Set-Alias -Name $Name -Value $Value -Scope $Scope
+    }
+}
+
+function Add-AzureAccounts
+{
+<#
+.SYNOPSIS
+Wraps Add-AzureAccount and Add-AzureRmAccount.
+
+.DESCRIPTION
+Logs you in ASM and ARM by wrapping Add-AzureAccount and Add-AzureRmAccount.
+
+.EXAMPLE
+Add-AzureAccounts
+This command will log you in both ASM and ARM.
+
+.NOTES
+Currently CmdletBinding only has internal support for -Verbose.
+#>
+
+    [CmdletBinding()] param()
+
+    Add-AzureAccount -Verbose:($PSBoundParameters["Verbose"].IsPresent -eq $true)
+    Add-AzureRmAccount -Verbose:($PSBoundParameters["Verbose"].IsPresent -eq $true)
+}
+
+function Switch-AzureSubscription
+{
+<#
+.SYNOPSIS
+Help method with UI (Out-GridView) for switching across Azure subscriptions.
+
+.DESCRIPTION
+Shows you the list of available subscriptions (Out-GridView) and then selects the chosen one in both ASM and ARM.
+
+.EXAMPLE
+Switch-AzureSubscription
+This command will show you the list of available subscriptions and then select the chosen one in both ASM and ARM.
+
+.NOTES
+Currently CmdletBinding only has internal support for -Verbose.
+#>
+
+    [CmdletBinding()] param()
+
+    $sub = Get-AzureRmSubscription | select SubscriptionName, SubscriptionId | Out-GridView -PassThru
+
+    Select-AzureSubscription -SubscriptionId $sub.SubscriptionId -Verbose:($PSBoundParameters["Verbose"].IsPresent -eq $true)
+    Select-AzureRmSubscription -SubscriptionId $sub.SubscriptionId -Verbose:($PSBoundParameters["Verbose"].IsPresent -eq $true)
+}
--- src/DevOpsFlex.Automation.PowerShell/DevOpsFlex.Automation.PowerShell/DevOpsFlex.Automation.PowerShell.psm1
+++ src/DevOpsFlex.Automation.PowerShell/DevOpsFlex.Automation.PowerShell/DevOpsFlex.Automation.PowerShell.psm1
@@ -1,9 +1,8 @@
-
 # Import functions
-$rootModulePath = Split-Path $script:MyInvocation.MyCommand.Path
 . "$PSScriptRoot\AzureADHelpers.ps1"
 . "$PSScriptRoot\AzurePrincipalWithCert.ps1"
 . "$PSScriptRoot\AzurePrincipalWithSecret.ps1"
+. "$PSScriptRoot\AzureProfileHelpers.ps1"
 . "$PSScriptRoot\AzureSubscriptionInKeyVault.ps1"
 . "$PSScriptRoot\ResizeASMDisk.ps1"
 
@@ -12,14 +11,23 @@ Export-ModuleMember -Function @(
     # AzureADHelpers
     'Get-MyUserObjectId'
     'Add-MeToKeyvault'
+
     # AzurePrincipalWithCert
     'New-AzurePrincipalWithCert'
     'Remove-AzurePrincipalWithCert'
+
     # AzurePrincipalWithSecret
     'New-AzurePrincipalWithSecret'
     'Remove-AzurePrincipalWithSecret'
+
+    # AzureProfileHelpers
+    'Reset-Alias'
+    'Add-AzureAccounts'
+    'Switch-AzureSubscription'
+
     # AzureSubscriptionInKeyVault
     'Register-AzureSubscriptionInKeyVault'
+
     # ResizeASMDisk
     'Set-AzureVMOSDiskSize'
     )
