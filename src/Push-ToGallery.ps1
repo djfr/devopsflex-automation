409be8fa58dd08511ee09e588ddf0f66a531ac3f
djfr <davidferreirarodrigues@gmail.com>
2016-11-11 12:37:08 +0000

    Fixed the publish script ... again ...

--- src/Push-ToGallery.ps1
+++ src/Push-ToGallery.ps1
@@ -1,19 +1,7 @@
 param
 (
     [parameter(Mandatory=$true, Position=0)]
-    [string] $ApiKey,
-
-    [parameter(Mandatory=$false, Position=1)]
-    [string] $FeedSource = $null
+    [string] $ApiKey
 )
 
-if(($FeedSource -ne $null) -and ((Get-PSRepository -Name EswPowerShell -ErrorAction SilentlyContinue) -eq $null)) {
-    Register-PSRepository -Name EswPowerShell -SourceLocation $FeedSource -PublishLocation "$FeedSource/package"
-}
-
-if($FeedSource -eq $null) {
-    Publish-Module -Name $PSScriptRoot\DevOpsFlex.Automation.PowerShell\DevOpsFlex.Automation.PowerShell.psd1 -NuGetApiKey $ApiKey
-}
-else {
-    Publish-Module -Name $PSScriptRoot\DevOpsFlex.Automation.PowerShell\DevOpsFlex.Automation.PowerShell.psd1 -Repository EswPowerShellDev -NuGetApiKey $ApiKey
-}
+Publish-Module -Name $PSScriptRoot\DevOpsFlex.Automation.PowerShell\DevOpsFlex.Automation.PowerShell.psd1 -NuGetApiKey $ApiKey
