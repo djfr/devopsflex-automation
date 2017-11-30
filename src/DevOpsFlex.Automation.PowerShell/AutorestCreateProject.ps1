2757d51cc49bbafef2434101466e33cee9d3d763
Vaclav Haken <vaclav@vaclavkabat.net>
2017-11-30 12:36:05 +0000

    VSTS1318

---
+++ src/DevOpsFlex.Automation.PowerShell/AutorestCreateProject.ps1
@@ -0,0 +1,42 @@
+﻿function Autorest-CreateProject
+{
+   	[CmdletBinding()]
+	param(
+	[string]$defUrl,
+	[string]$namespace,
+	[string]$outputFolder="output",
+	[string]$lang="csharp",
+	[string]$config="release"
+	)
+     try
+     {	
+		if (!$defUrl)
+		{
+			throw "Missing parameter value for definition file URL"
+		}
+
+		if (!$namespace)
+		{
+			throw "Missing parameter value for namespace"
+		}
+
+		autorest --latest
+
+		"Definition URL :$defUrl"
+		"Language :$lang"
+		"Output folder :$outputFolder"
+		"Configuration :$config"
+
+		iwr $defUrl -o definition.json
+		autorest --input-file=definition.json --$lang --output-folder=$outputFolder --namespace=$namespace
+		dotnet autorest-createproject -s definition.json -o $outputFolder
+
+		pushd $outputFolder
+		dotnet build -c $config
+		popd
+    }
+    catch
+    {
+        throw $_.Exception.Message       
+    }	
+}
