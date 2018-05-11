1f44750475c53add635dcdbe6999992ef5bf3a8a
djfr <davidferreirarodrigues@gmail.com>
2018-05-11 15:47:07 +0100

    Fixed several issues with the app gateway setup

--- src/DevOpsFlex.Automation.PowerShell/FabricEndpoints.ps1
+++ src/DevOpsFlex.Automation.PowerShell/FabricEndpoints.ps1
@@ -26,57 +26,64 @@
         $dnsLayerSuffix = ""
     }
 
-    $lbs | % {
-        ### MOVE THIS INTO IT'S OWN THING
-        # Find the Configuration / DNS record settings
-        $_.Name -match '\w*-(\w*)-\w*-(\w*)-\w*-\w*' > $null
-        $region = $Matches[1]
-        $configuration = $Matches[2]
+#    $lbs | % {
+#        ### MOVE THIS INTO IT'S OWN THING
+#        # Find the Configuration / DNS record settings
+#        $_.Name -match '\w*-(\w*)-\w*-(\w*)-\w*-\w*' > $null
+#        $region = $Matches[1]
+#        $configuration = $Matches[2]
+#
+#        if($configuration -eq 'prod' -or $configuration -eq 'sand') {
+#            $dnsSuffix = 'com'
+#        }
+#        else {
+#            $dnsSuffix = 'net'
+#        }
+#
+#        if($lbs.Count -gt 1) {
+#            $dnsName = "$Name-$region$dnsLayerSuffix"
+#        }
+#        else {
+#            $dnsName = "$Name$dnsLayerSuffix"
+#        }
+#        ###
+#
+#        # Find the public IP address of the load balancer
+#        if($UseSsl.IsPresent) {
+#            $pip = ($_.FrontendIpConfigurations)[0].PrivateIpAddress
+#        }
+#        else {
+#            $pipRes = Get-AzureRmResource -ResourceId ($_.FrontendIpConfigurations[0].PublicIpAddress.Id)
+#            $pip = (Get-AzureRmPublicIpAddress -Name $pipRes.ResourceName -ResourceGroupName $pipRes.ResourceGroupName).IpAddress
+#        }
+#
+#        New-AzureRmDnsRecordSet -Name "$dnsName" `
+#                                -RecordType A `
+#                                -ZoneName "$configuration.eshopworld.$dnsSuffix" `
+#                                -ResourceGroupName "global-platform-$configuration" `
+#                                -Ttl 360 `
+#                                -DnsRecords (New-AzureRmDnsRecordConfig -IPv4Address "$pip") > $null
+#
+#        $_ | Add-AzureRmLoadBalancerProbeConfig -Name "$Name" `
+#                                                -Protocol Http `
+#                                                -Port $Port `
+#                                                -RequestPath $ProbePath `
+#                                                -IntervalInSeconds 30 `
+#                                                -ProbeCount 2 > $null
+#        $_ | Set-AzureRmLoadBalancer > $null
+#
+#        $probeId = ((Get-AzureRmLoadBalancer -Name $_.Name -ResourceGroupName $_.ResourceGroupName).Probes | ? { $_.Name -match $Name})[0].Id
+#        $_ | Add-AzureRmLoadBalancerRuleConfig -Name "$Name" `
+#                                               -Protocol Tcp `
+#                                               -ProbeId $probeId `
+#                                               -FrontendPort $Port `
+#                                               -BackendPort $Port `
+#                                               -FrontendIpConfigurationId $_.FrontendIpConfigurations[0].Id `
+#                                               -BackendAddressPoolId $_.BackendAddressPools[0].Id > $null
+#        $_ | Set-AzureRmLoadBalancer > $null
+#    }
 
-        if($configuration -eq 'prod' -or $configuration -eq 'sand') {
-            $dnsSuffix = 'com'
-        }
-        else {
-            $dnsSuffix = 'net'
-        }
-
-        if($lbs.Count -gt 1) {
-            $dnsName = "$Name-$region$dnsLayerSuffix"
-        }
-        else {
-            $dnsName = "$Name$dnsLayerSuffix"
-        }
-        ###
-
-        # Find the public IP address of the load balancer
-        $pipRes = Get-AzureRmResource -ResourceId ($_.FrontendIpConfigurations[0].PublicIpAddress.Id)
-        $pip = (Get-AzureRmPublicIpAddress -Name $pipRes.ResourceName -ResourceGroupName $pipRes.ResourceGroupName).IpAddress
-
-        New-AzureRmDnsRecordSet -Name "$dnsName" `
-                                -RecordType A `
-                                -ZoneName "$configuration.eshopworld.$dnsSuffix" `
-                                -ResourceGroupName "global-platform-$configuration" `
-                                -Ttl 360 `
-                                -DnsRecords (New-AzureRmDnsRecordConfig -IPv4Address "$pip") > $null
-
-        $_ | Add-AzureRmLoadBalancerProbeConfig -Name "$Name" `
-                                                -Protocol Http `
-                                                -Port $Port `
-                                                -RequestPath $ProbePath `
-                                                -IntervalInSeconds 30 `
-                                                -ProbeCount 2 > $null
-        $_ | Set-AzureRmLoadBalancer > $null
-
-        $probeId = ((Get-AzureRmLoadBalancer -Name $_.Name -ResourceGroupName $_.ResourceGroupName).Probes | ? { $_.Name -match $Name})[0].Id
-        $_ | Add-AzureRmLoadBalancerRuleConfig -Name "$Name" `
-                                               -Protocol Tcp `
-                                               -ProbeId $probeId `
-                                               -FrontendPort $Port `
-                                               -BackendPort $Port `
-                                               -FrontendIpConfigurationId $_.FrontendIpConfigurations[0].Id `
-                                               -BackendAddressPoolId $_.BackendAddressPools[0].Id > $null
-        $_ | Set-AzureRmLoadBalancer > $null
-    }
+    Write-Host 'Done with LBs'
 
     if($UseSsl.IsPresent) {
         $appGateways = Get-AzureRmApplicationGateway
@@ -103,6 +110,9 @@
             }
             ###
 
+            ### MISSING THE PORT CHECK -> ADD THIS AFTER THIS THING ACTUALLY WORKS!
+            # $ag | Add-AzureRmApplicationGatewayFrontendPort -Name https-port -Port 443 | Set-AzureRmApplicationGateway
+
             # Find the public IP address of the app gateway
             $pipRes = Get-AzureRmResource -ResourceId ($_.FrontendIPConfigurations[0].PublicIPAddress.Id)
             $pip = (Get-AzureRmPublicIpAddress -Name $pipRes.ResourceName -ResourceGroupName $pipRes.ResourceGroupName).IpAddress
@@ -127,7 +137,7 @@
                                                            -Protocol "Https" `
                                                            -SslCertificateId ($foo.SslCertificates | ? { $_.Name -match "star.eshopworld.net" })[0].Id `
                                                            -FrontendIPConfigurationId $foo.FrontendIPConfigurations[0].Id `
-                                                           -FrontendPort 443 `
+                                                           -FrontendPort ($_.FrontendPorts | ? { $_.Port -eq 443 })[0].Id `
                                                            -HostName "$dnsName.$configuration.eshopworld.$dnsSuffix"
             $_ | Set-AzureRmApplicationGateway
 
@@ -137,14 +147,20 @@
                                                                   -Protocol "HTTP" `
                                                                   -ProbeId $probeId `
                                                                   -CookieBasedAffinity "Disabled"
-            $appGateway = $_ | Set-AzureRmApplicationGateway
-
+            $_ | Set-AzureRmApplicationGateway
+            
+            $appGateway = Get-AzureRmApplicationGateway -Name $_.Name -ResourceGroupName $_.ResourceGroupName
             $_ | Add-AzureApplicationGatewayRequestRoutingRule -Name $Name `
                                                                -RuleType Basic `
                                                                -BackendHttpSettingsId ($appGateway.BackendHttpSettingsCollection | ? { $_.Name -match $Name })[0].Id `
                                                                -HttpListenerId ($appGateway.HttpListeners | ? { $_.Name -match $Name })[0].Id `
                                                                -BackendAddressPool ($appGateway.BackendAddressPools)[0].Id
             $_ | Set-AzureRmApplicationGateway
+
         }
+
+        Write-Host 'Done with LBs'
     }
+
+    Write-Host 'Done with everything'
 }
